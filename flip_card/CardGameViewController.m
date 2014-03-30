85774474ea0ff4b77efef6a5304cfe8ce98ccb64
GWseo <seogw@hotmail.com>
2014-03-31 00:31:17 +0900

    count flip

--- flip_card/CardGameViewController.m
+++ flip_card/CardGameViewController.m
@@ -9,10 +9,17 @@
 #import "CardGameViewController.h"
 
 @interface CardGameViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) int flipCount;
 @end
 
 @implementation CardGameViewController
+- (void)setFlipCount:(int)flipCount
+{
+    _flipCount = flipCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
+    NSLog(@"flipCount changed to %d", self.flipCount);
+}
 - (IBAction)touchCardButton:(UIButton *)sender {
     if([sender.currentTitle length]){
         [sender setBackgroundImage:[UIImage imageNamed:@"card_background"] forState:UIControlStateNormal];
@@ -21,7 +28,7 @@
         [sender setBackgroundImage:[UIImage imageNamed:@"card_front"] forState:UIControlStateNormal];
         [sender setTitle:@"A♣︎" forState:UIControlStateNormal];
     }
-    
+    self.flipCount++;
 }
 
 
